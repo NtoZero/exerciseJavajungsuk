ed04ab7b4c0219c779432ffabd097f042ddffb04
LST <dev.st1027@gmail.com>
2023-03-20 11:27:49 +0900

    CHORE: 챕터 8 문제 수정

--- ch08_Wed_Exercise_src/src/Exercise8_3.java
+++ ch08_Wed_Exercise_src/src/Exercise8_3.java
@@ -3,7 +3,11 @@
 //				throws InvalidNumberException, NotANumberException {}
 //
 //		class NumberException extends Exception {}
-//		class InvalidNumberException extends NumberException {} 
+//		class InvalidNumberException extends NumberException {}
 //		class NotANumberException extends NumberException {}
-//		
-//		
+//
+//        a.    void add(int a, int b) throws InvalidNumberException, NotANumberException {}
+//        b.	void add(int a, int b) throws InvalidNumberException {}
+//        c.	void add(int a, int b) throws NotANumberException {}
+//        d.	void add(int a, int b) throws Exception {}
+//        e.	void add(int a, int b) throws NumberException {}
