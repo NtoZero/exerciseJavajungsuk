6843a701960b501c4ea6524129b8317282a840c4
LST <dev.st1027@gmail.com>
2023-04-12 20:17:44 +0900

    CHORE: 12챕터 중복 문제  삭제

--- Exercise_JJ3rd/ch12_Wed_Exercise_src/src/Exercise12_3.java
+++
@@ -1,9 +0,0 @@
-////[12-3] 다음 중 올바르지 않은 문장을 모두 고르시오.
-//
-//        a.	Box<?> b = new Box();
-//        b.	Box<?> b = new Box<>();
-//        c.	Box<?> b = new Box<Object>();
-//        d.	Box<Object> b = new Box<Fruit>();
-//        e.	Box	b = new Box<Fruit>();
-//        f.	Box<? extends Fruit> b = new Box<Apple>();
-//        g.	Box<? extends Object> b = new Box<? extends Fruit>();
