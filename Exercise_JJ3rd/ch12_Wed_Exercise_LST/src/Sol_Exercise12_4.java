62f18cae58a93b0873ba84a3de9ec5895385a537
LST <dev.st1027@gmail.com>
2023-04-12 20:10:51 +0900

    CHORE: 문제풀이 폴더 12챕터 추가

---
+++ Exercise_JJ3rd/ch12_Wed_Exercise_LST/src/Sol_Exercise12_4.java
@@ -0,0 +1,19 @@
+//[12-4] 아래의 메서드는 두 개의 ArrayList를 매개변수로 받아서,
+//        하나의 새로운 ArrayList로 병합하는 메서드이다.
+//        이를 지네릭 메서드로 변경하시오.
+
+public static ArrayList<?extends Product> merge( ArrayList<?extends Product> list, ArrayList<?extends Product> list2)
+        {
+            ArrayList<?extends Product> newList = new ArrayList<>(list);
+            newList.addAll(list2);
+            return newList;
+        }
+
+
+/*
+<정답>
+public static <T extends Product> ArrayList<T> merge( ArrayList<T> list, ArrayList<T> list2) {
+        ArrayList<T> newList = new ArrayList<>(list); newList.addAll(list2);
+        return newList;
+        }
+*/
